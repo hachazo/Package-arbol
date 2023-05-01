b02d592f9d9a8ffab5d1817c725ae7931fd5d5dd
hachazo <87842940+hachazo@users.noreply.github.com>
2023-04-30 21:36:02 -0300

    update

---
+++ arbol.ads
@@ -0,0 +1,32 @@
+generic
+   
+   type Tipoelem is private;
+   with function "<" (X, Y: Tipoelem) return Boolean;
+   with function ">" (X, Y: Tipoelem) return Boolean;
+   with procedure Put (X: in TipoElem);
+   
+   package Arbol is
+      
+      type Tipoarbol is private;
+      
+      function Vacio (Raiz: Tipoarbol) return Boolean;
+      procedure Insertar(Raiz: in out Tipoarbol; Elemento: in Tipoelem);
+      procedure Suprimir(Arbol: in out TipoArbol; ValSup: in TipoElem);
+      function Esta(Raiz: in TipoArbol; Buscado: in TipoElem) return Boolean;
+      procedure Limpiar(Ptr: in out TipoArbol);
+      function Izq(Ptr: TipoArbol) return TipoArbol;
+      function Der(Ptr: TipoArbol) return TipoArbol;
+      function Info(Ptr: TipoArbol) return TipoElem;
+      Arbolvacio: exception;
+   
+      private
+      
+      type Tiponodo;
+      type Tipoarbol is access Tiponodo;
+      type Tiponodo is
+      record
+         Info: Tipoelem;
+         Izq, Der: Tipoarbol;
+   end record;
+   
+end arbol;
